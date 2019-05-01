bc2275d18d8ed68ca8f1cbd6d178a10c1dab8906
uvafan <eli.d.lifland@gmail.com>
2019-04-30 21:25:06 -0400

    station info

--- plot_stations_vs_nodes.py
+++ plot_stations_vs_nodes.py
@@ -2,24 +2,8 @@ import pandas as pd
 import sc_plot
 
 def main():
-    '''
-    stations used, in order:
-    Chicago Loop
-    West Town
-    Wicker Park
-    Logan Square
-    Ukranian Village
-    United Center
-    Troposphere Group
-    West Loop
-    Lakeshore East
-    West Loop - Union Park
-    '''
-    station_df = pd.DataFrame({
-    'lat': [41.88,41.89,41.91,41.91,41.89,41.88,41.87,41.88,41.89,41.88],
-    'lon': [-87.63,-87.65,-87.68,-87.7,-87.69,-87.68,-87.63,-87.65,-87.62,-87.66]
-    })
-    node_df = pd.read_csv('../../AoT_Chicago.complete.2019-03-28/nodes.csv')
+    station_df = pd.read_csv('stations.csv')
+    node_df = pd.read_csv('../Spatial_STL_SC/chicago_data_2018-10-08/nodes.csv')
     sc_plot.plotTwoGroups(station_df,node_df)
 
 main()
--- scrape_wund.py
+++ scrape_wund.py
@@ -8,13 +8,15 @@ from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
 from datetime import timedelta,date
 
+DATA_DIR = 'data2'
+
 def daterange(start_date,end_date):
     for n in range(int((end_date-start_date).days)):
         yield start_date + timedelta(n)
 
 def main():
     start_date = date(2018,1,3)
-    end_date = date(2019,4,1)
+    end_date = date(2018,10,6)
     driver = webdriver.Chrome('../chromedriver/chromedriver')
     for single_date in daterange(start_date,end_date):
         scrape_day(single_date.month,single_date.day,single_date.year,driver)
@@ -22,11 +24,14 @@ def main():
 def scrape_day(month,day,year,driver):
     url = 'https://www.wunderground.com/history/daily/us/il/des-plaines/KORD/date/{}-{}-{}'.format(year,month,day)
     driver.get(url)
-    tables = WebDriverWait(driver,20).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "table")))
-    for table in tables:
-        newTable = pd.read_html(table.get_attribute('outerHTML'))
-        if newTable:
-            newTable = newTable[0].fillna('')
-            newTable.to_csv('data/{}-{}-{}.csv'.format(month,day,year))
+    done = False
+    while not done:
+        tables = WebDriverWait(driver,100).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "table")))
+        for table in tables:
+            newTable = pd.read_html(table.get_attribute('outerHTML'))
+            if newTable:
+                newTable = newTable[0].fillna('')
+                newTable.to_csv('{}/{}-{}-{}.csv'.format(DATA_DIR,month,day,year))
+                done = True
 
 main()
